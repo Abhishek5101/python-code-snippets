e75b590cb49b9f0c44e0324db5f6bbc1a6ec3c64
Abhishek Kulkarni <miabhishekkulkarni@gmail.com>
2019-06-30 01:45:47 +0530

    understanding datetime module in python

--- date and time.py
+++ date and time.py
@@ -1,4 +1,5 @@
 import datetime
+import pytz
 
 date = datetime.date.today()
 #print(date.isoweekday())  #Monday - 1
@@ -12,4 +13,21 @@ today = datetime.date.today()
 
 
 diff = bday - today
-print(diff.total_seconds())
+#print(diff.total_seconds())
+
+
+current_time = datetime.time(23, 23, 23, 23)
+#print(current_time.hour)
+
+
+dt = datetime.datetime(2019, 7, 27, 12, 30, 45, tzinfo=pytz.UTC)
+print(dt)
+
+dt_now = datetime.datetime.now(tz=pytz.UTC)
+print(dt_now)
+
+dt_utcnow = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+print(dt_utcnow)
+
+
+
