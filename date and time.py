42360e502ef8c2af37f6ca81f83c4a37090d387b
Abhishek Kulkarni <miabhishekkulkarni@gmail.com>
2019-06-30 01:09:27 +0530

    understanding datetime module in python

---
+++ date and time.py
@@ -0,0 +1,4 @@
+import datetime
+
+date = datetime.date(2202, 6, 14)
+print(date)
--- magic methods.py
+++ magic methods.py
@@ -5,13 +5,15 @@ class Letter:
     def __str__(self):
         morse = []
         for i in self.pattern:
-            if i == "."
+            if i == ".":
                 morse.append("Dot")
             elif i == "_":
                 morse.append("Dash")
         return "-".join(morse)
 
+
 class S(Letter):
     def __init__(self):
         pattern = ['.', '.', '.']
         super().__init__(pattern)
+
