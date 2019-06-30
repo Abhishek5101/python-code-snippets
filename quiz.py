caac31fc0163512591c3bada2199ffcb1b4ed00d
Abhishek Kulkarni <miabhishekkulkarni@gmail.com>
2019-06-30 17:44:18 +0530

    building a timed quiz

--- quiz.py
+++ quiz.py
@@ -2,3 +2,18 @@ import datetime
 import random
 
 from questions import Add, Multiply
+
+
+class Quiz:
+    questions = []
+    def __init__(self):
+        pass
+
+    def take_quiz(self):
+        pass
+
+    def ask(self, question):
+        pass
+
+    def summary(self):
+        pass
