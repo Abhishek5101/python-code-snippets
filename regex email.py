7b0907e3f849cfeeb575ea76b4daa45933ab5b7b
Abhishek Kulkarni <miabhishekkulkarni@gmail.com>
2019-07-03 18:59:10 +0530

    using regex to find emails in data

--- regex email.py
+++ regex email.py
@@ -4,3 +4,6 @@ import re
 # >>> find_email("kenneth.love@teamtreehouse.com, @support, ryan@teamtreehouse.com, test+case@example.co.uk")
 # ['kenneth@teamtreehouse.com', 'ryan@teamtreehouse.com', 'test@example.co.uk']
 
+
+def find_emails(string):
+    return re.findall(r'[-\w+.]+@[-\w.]+', string)
