c26f8fed531c213f4222032b1f10d76b8a738211
Abhishek Kulkarni <miabhishekkulkarni@gmail.com>
2019-07-09 19:12:14 +0530

    creating a Yatzy game

---
+++ dice.py
@@ -0,0 +1,15 @@
+import random
+
+
+class Die:
+    def __init__(self, sides=2, value=0):
+        if not sides >= 2 or not isinstance(sides, int):
+            raise ValueError("Sides should be a whole number greater than 2")
+        self.value = value or random.choice(1, sides)
+        if not isinstance(value, int):
+            raise ValueError("Values need to be a whole number")
+
+
+class D6(Die):
+    def __init__(self, value=0):
+        super().__init__(sides=6, value=value)
