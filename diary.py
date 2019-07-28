21d80c15777ffe49993944215e53d17874798ebd
Abhishek Kulkarni <miabhishekkulkarni@gmail.com>
2019-07-28 11:50:04 +0530

    set up for diary app

--- diary.py
+++ diary.py
@@ -2,6 +2,7 @@ from peewee import *
 
 db = SqliteDatabase('diary.db')
 
+
 class Entry(Model):
     """
     contnet and timestamp
@@ -9,3 +10,19 @@ class Entry(Model):
 
     class Meta:
         database = db
+
+
+def add_entry():
+    pass
+
+
+def view_entries():
+    pass
+
+
+def delete_entry():
+    pass
+
+
+if __name__ == "__main__":
+    menu_loop()
